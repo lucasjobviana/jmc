1feff5f79c96e5e2e48e20d86c1386d79b76ba29
Lucas Job Viana <lucasjobvianna@gmail.com>
2024-01-30 01:07:53 -0300

    :sparkles: feat: Adicionado VehicleRepository.

---
+++ src/main/java/com/job/jmc/api/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.job.jmc.api.repository;
+
+import com.job.jmc.api.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+}

cd205b1b9868858276889fd76edac043f1e8cb1f
Lucas Job Viana <lucasjobvianna@gmail.com>
2024-01-30 01:18:13 -0300

    :sparkles: feat: Adicionado VehicleService.

---
+++ src/main/java/com/job/jmc/api/service/VehicleService.java
@@ -0,0 +1,8 @@
+package com.job.jmc.api.service;
+
+import com.job.jmc.api.repository.VehicleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+}

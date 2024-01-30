43ebc7e9249308a443062ebc8e4980865261a56d
Lucas Job Viana <lucasjobvianna@gmail.com>
2024-01-30 01:11:26 -0300

    :sparkles: feat: Adiciona VehicleController.

---
+++ src/main/java/com/job/jmc/api/controller/VehicleController.java
@@ -0,0 +1,12 @@
+package com.job.jmc.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/vehicles")
+public class VehicleController {
+}
